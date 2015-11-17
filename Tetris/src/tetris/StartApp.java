3fa3734686e832fb10f9b123df920bd5d9b004a9
aMiller28 <awm21@students.uwf.edu>
2015-11-17 16:44:47 -0600

    Test
    Tetris test file

---
+++ Tetris/src/tetris/StartApp.java
@@ -0,0 +1,21 @@
+package tetris;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class StartApp extends Application
+{
+	@Override
+	public void start(Stage primaryStage) 
+	{
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+}
